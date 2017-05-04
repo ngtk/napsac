d9bfbb1bd69f12e55ff6554781f1773aa499325f
Kento Nagata <ngtk@users.noreply.github.com>
2017-05-05 00:58:40 +0900

    Update play.sh

--- provisioning/play.sh
+++ provisioning/play.sh
@@ -10,8 +10,14 @@ done
 
 cd "$(dirname $0)"
 
+if ! [ -f ./secrets.yml ]; then
+  cp ./secrets{.sample,}.yml
+  [ "${DOTFILES_NOEDIT_SECRETS:-0}" -eq 0 ] && vim ./secrets.yml
+fi
+
 ansible-playbook \
   -i 'localhost' \
   --extra-vars='@config.yml' \
+  --extra-vars='@secrets.yml' \
   ${ARGS[@]} \
-  playbook.yml \
+  playbook.yml
